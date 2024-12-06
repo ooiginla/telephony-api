421f8219f43507a840ad163afc1c53314680826a
Iginla omotayo <oiginla@axxess.com>
2024-12-06 19:13:20 +0100

    update visit status property

--- app/Services/Continulink.php
+++ app/Services/Continulink.php
@@ -241,7 +241,7 @@ class Continulink
             $visit->visit_end = $this->convertVisitDate($schedule['end']) ?? null;
             $visit->visit_type = $schedule['type'] ?? '';
             $visit->schedule_type = $schedule['schedule_type'] ?? '';
-            $visit->status = (strtolower($schedule['active'])) == "true" ? true : false;
+            $visit->status = ($schedule['active']) ? true : false;
             $visit->profile_id = $this->profile->id;
             $visit->save();
 
